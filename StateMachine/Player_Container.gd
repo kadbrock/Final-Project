a93c760fff2401d0ad350748fda181c7c57b50a8
ArcPR <arcprax@gmail.com>
2023-04-18 22:31:40 -0400

    commit (camera works now)

--- StateMachine/Player_Container.gd
+++ StateMachine/Player_Container.gd
@@ -5,7 +5,9 @@ var starting_position = Vector2(200,200)
 
 
 func _physics_process(_delta):
-	if not has_node("Player"):
-		var player = Player.instance()
+	var player = get_node_or_null("Player")
+	if player == null:
+		player = Player.instance()
 		player.position = starting_position
+		player.name = "Player"
 		add_child(player)
