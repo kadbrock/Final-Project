fd1a3569171cf3ab823e9563a6491b34cfdbda96
nkintner <nkintner@iu.edu>
2023-04-20 00:39:46 -0400

    Adds Sound Effects
    Adds king death
    Adds Jump Sound when standing still
    Adds ambient Music for first level

--- StateMachine/Jumping.gd
+++ StateMachine/Jumping.gd
@@ -14,14 +14,15 @@ func physics_process(_delta):
 	if not player.is_on_floor():
 		player.velocity += player.move_speed * player.move_vector() + player.gravity
 		player.move_and_slide(player.velocity, Vector2.UP)
+		
 	if player.is_moving():
 		player.jump_power.x = clamp(player.jump_power.x + (player.move_vector().x * player.leap_speed), -player.max_leap, player.max_leap)
 	if Input.is_action_pressed("jump"):
 		player.jump_power.y = clamp(player.jump_power.y - player.jump_speed, -player.max_jump, 0)
+		
 	else:
 		player.velocity.y = 0
 		player.velocity += player.jump_power
 		player.move_and_slide(player.velocity, Vector2.UP)
 		SM.set_state("Falling")
-		
-
+		$Jump.play()
